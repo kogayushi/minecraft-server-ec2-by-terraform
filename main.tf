f100cece850f005cd81c30a9c3527df597c99e32
koga yushi <youshe.old.pleasure@gmail.com>
2024-12-30 16:04:05 +0900

    specify security group name

--- main.tf
+++ main.tf
@@ -23,6 +23,10 @@ resource "aws_security_group" "minecraft_server" {
     protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "minecraft-server"
+  }
 }
 
 resource "aws_instance" "minecraft_server" {
